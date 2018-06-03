8df39fc6445d4957fb9238a5bbfa46bea2bdac83
kimsphere <kimiley90@gmail.com>
2018-06-03 23:54:11 +0800

    [AK903] Changed auth middleware from all controllers to route - Removed all repeated middleware constructs from controllers

--- app/Http/Controllers/Menu/DashboardController.php
+++ app/Http/Controllers/Menu/DashboardController.php
@@ -9,16 +9,6 @@ class DashboardController extends Controller
 {
 
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
--- app/Http/Controllers/Menu/SupplierController.php
+++ app/Http/Controllers/Menu/SupplierController.php
@@ -7,18 +7,6 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Display a listing of the resource.
      *
