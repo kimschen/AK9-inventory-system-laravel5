f680f987c0304d493a0f82711c4439fae1eee5f7
kimsphere <kimiley90@gmail.com>
2018-07-12 20:10:55 +0800

    [AK907] Changed product display horizontally

--- resources/views/menu/inventory/index.blade.php
+++ resources/views/menu/inventory/index.blade.php
@@ -4,28 +4,35 @@
 @section('content')
 
 <div class="container">
-	<button class="btn btn-primary float-right" onclick="window.location='{{ route('create') }}';">Add Product</button>
+	<button class="btn btn-primary float-right mt-3" onclick="window.location='{{ route('create') }}';">Add Product</button>
 </div>
 
-@foreach($products as $product)
-<div class="container">
-	<div class="card" style="width: 12rem;">
-	 	<img class="card-img-top" src="{{ $product->image_path}}" alt="product-image">
-		<ul class="list-group list-group-flush">
-	    	<li class="list-group-item">{{ $product->name }}</li>
-	    	<li class="list-group-item">{{ $product->unit_cost }}</li>
-	    	<li class="list-group-item">{{ $product->quantity }}</li>
-		</ul>
-		<div class="container">
-		<button type="submit" class="btn btn-secondary pl-2 pr-2 m-1" onclick="window.location='{{ action('Menu\InventoryController@edit', $product->id) }}';">Edit</button>
-		<form method="post" action="{{ action('Menu\InventoryController@destroy', $product->id) }}">
-			{{ csrf_field() }}
-			<input name="_method" type="hidden" value="DELETE">
-			<button type="submit" class="btn btn-danger pl-2 pr-2 m-1">Delete</button>
-		</form>
+<div class="container row">
+	@foreach($products as $product)
+	<div class="col-lg-2 mt-4">
+		<div class="card">
+		 	<img class="card-img-top" src="{{ $product->image_path}}" alt="product-image">
+			<ul class="list-group list-group-flush">
+		    	<li class="list-group-item">{{ $product->name }}</li>
+		    	<li class="list-group-item">{{ $product->unit_cost }}</li>
+		    	<li class="list-group-item">{{ $product->quantity }}</li>
+			</ul>
+
+			<div class="container row align-self-center">
+				<button type="submit"
+						class="btn btn-secondary pl-2 pr-2 m-1"
+						onclick="window.location='{{ action('Menu\InventoryController@edit', $product->id) }}';">Edit
+				</button>
+				<form method="post" action="{{ action('Menu\InventoryController@destroy', $product->id) }}">
+					{{ csrf_field() }}
+					<input name="_method" type="hidden" value="DELETE">
+					<button type="submit" class="btn btn-danger pl-1 pr-1 m-1">Delete</button>
+				</form>
+			</div>
+
 		</div>
 	</div>
+	@endforeach
 </div>
 
-@endforeach
 @endsection
