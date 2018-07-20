9a8c098354195057b0a7b7b453991bc6b1fe4ec1
kimsphere <kimiley90@gmail.com>
2018-07-21 01:56:46 +0800

    [AK907] Changed product's edit and delete button display - Edit and Delete buttons only appear when mouse hover on the product image

--- resources/views/menu/inventory/index.blade.php
+++ resources/views/menu/inventory/index.blade.php
@@ -10,24 +10,26 @@
 	@foreach($products as $product)
 	<div class="col-lg-2 mt-4">
 		<div class="card">
-		 	<img class="card-img-top" src="{{ $product->image_path}}" alt="product-image" width="150px" height="150px">
+		 	<div class="product-image">
+		 		<img class="card-img-top" src="{{ $product->image_path}}" alt="product-image" width="150px" height="150px">
+		 	</div>
+			<div class="container row align-self-center product-button">
+				<button type="submit"
+						class="btn pl-2 pr-2 m-1"
+						onclick="window.location='{{ action('Menu\InventoryController@edit', $product->id) }}';">Edit
+				</button>
+				<form method="post" action="{{ action('Menu\InventoryController@destroy', $product->id) }}">
+					{{ csrf_field() }}
+					<input name="_method" type="hidden" value="DELETE">
+					<button type="submit" class="btn pl-1 pr-1 m-1">Delete</button>
+				</form>
+			</div>
 			<ul class="list-group list-group-flush">
 		    	<li class="list-group-item">{{ $product->name }}</li>
 		    	<li class="list-group-item">MYR {{ $product->unit_cost }}</li>
 		    	<li class="list-group-item">{{ $product->quantity }}</li>
 			</ul>
 
-			<div class="container row align-self-center">
-				<button type="submit"
-						class="btn btn-secondary pl-2 pr-2 m-1"
-						onclick="window.location='{{ action('Menu\InventoryController@edit', $product->id) }}';">Edit
-				</button>
-				<form method="post" action="{{ action('Menu\InventoryController@destroy', $product->id) }}">
-					{{ csrf_field() }}
-					<input name="_method" type="hidden" value="DELETE">
-					<button type="submit" class="btn btn-danger pl-1 pr-1 m-1">Delete</button>
-				</form>
-			</div>
 
 		</div>
 	</div>
